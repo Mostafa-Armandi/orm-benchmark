ff7e8cb1279911429d61bb6c9ec4e2dc8585261c
Mostafa Armandi <m.armandi@gmail.com>
2022-09-11 22:37:25 +0100

    change running model

--- ORMBenchmark/Program.cs
+++ ORMBenchmark/Program.cs
@@ -1,4 +1,3 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+BenchmarkRunner.Run<ORMBenchmark.Benchmarks>();
